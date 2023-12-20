325858ecf64ac36ec84ffa474a13ab0e0240b00c
sobanimran <soubansheikh163@gmail.com>
2023-12-21 02:27:10 +0500

    first commit

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import { CldUploadButton } from 'next-cloudinary';
+
+
+const Gallery = () => {
+  return (
+    <section>
+      <div className='flex justify-between'>
+
+        <h1 className="text-4xl font-bold">
+          Gallery
+        </h1>
+        <Button asChild >
+          <div className='flex gap-2'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+          </svg>
+
+          <CldUploadButton
+            onUpload={(result) => {
+
+            }}
+            uploadPreset="<Upload Preset>" />
+            </div>
+        </Button>
+      </div>
+    </section>
+  )
+}
+
+export default Gallery
